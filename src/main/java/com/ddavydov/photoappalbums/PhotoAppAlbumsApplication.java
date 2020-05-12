b85d8ef5b8a661fc131d34e4433b6fa22e268db8
DMdavydov <dmitry_davydov1@epam.com>
2020-05-12 11:50:06 +0300

    Initial commit

---
+++ src/main/java/com/ddavydov/photoappalbums/PhotoAppAlbumsApplication.java
@@ -0,0 +1,13 @@
+package com.ddavydov.photoappalbums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppAlbumsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppAlbumsApplication.class, args);
+    }
+
+}
